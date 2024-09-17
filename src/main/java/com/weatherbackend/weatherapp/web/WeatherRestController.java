32bbe7a9c68257b445c1440ec46d6e50bacb3f8c
joakha <johannes.haapanen2@gmail.com>
2024-09-17 12:33:25 +0300

    fix cors problem

--- src/main/java/com/weatherbackend/weatherapp/web/WeatherRestController.java
+++ src/main/java/com/weatherbackend/weatherapp/web/WeatherRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.weatherbackend.weatherapp.domain.Weather;
 import com.weatherbackend.weatherapp.domain.WeatherRepository;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,6 +22,7 @@ public class WeatherRestController {
 
     // all weathers
     @GetMapping("/api/weathers")
+    @CrossOrigin(origins = "http://localhost:5173")
     public Iterable<Weather> getWeathers() {
 
         return weatherRepository.findAll();
@@ -28,6 +30,7 @@ public class WeatherRestController {
 
     // weather by id
     @GetMapping("/api/weathers/{id}")
+    @CrossOrigin(origins = "http://localhost:5173")
     public @ResponseBody Optional<Weather> getWeatherById(@PathVariable("id") Long id) {
 
         return weatherRepository.findById(id);
