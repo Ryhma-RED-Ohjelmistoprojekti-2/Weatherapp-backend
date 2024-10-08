ba3bce6decce52397e9d7bd34d689f69fc5fa954
Kaj Jansson <kaj.jansson@myy.haaga-helia.fi>
2024-10-08 10:24:53 +0300

    Added comments to WebSecurityConfig

--- src/main/java/com/weatherbackend/weatherapp/WebSecurityConfig.java
+++ src/main/java/com/weatherbackend/weatherapp/WebSecurityConfig.java
@@ -19,10 +19,30 @@ public class WebSecurityConfig {
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
     http.authorizeHttpRequests(authorize -> authorize
-      .requestMatchers(antMatcher("/api/**")).permitAll()
-    );
+        .requestMatchers(antMatcher("/api/**")).permitAll());
     return http.build();
   }
+
+  /*
+   * According to Copilot:
+   * 
+   * WebMvcConfigurer (interface in Spring Framework) customises default configs
+   * of Spring MVC.
+   * 
+   * Options addCorsMappings, addMapping and allowedOrigins configures
+   * how the backend interacts with frontend and softala.haaga-helia.fi .
+   * 
+   * Particularly in therms of handling Cross-Origin Resource Sharing (CORS)
+   * [a security feature implemented by browsers to prevent web pages from
+   * requesting to another domain] and API endpoint mappings.
+   * 
+   * In other words, these are setting up the backend to allow
+   * the frontend (URL) and softala.haaga-helia.fi to make requests
+   * to the backend API endpoints. Very crucial when they are all
+   * hosted on different domains.
+   * 
+   * api/** means that configs will be applied to all API endpoints.
+   */
 
   @Bean
   public WebMvcConfigurer corsConfigurer() {
@@ -30,15 +50,16 @@ public class WebSecurityConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-        .allowedOrigins(
-          "https://softala.haaga-helia.fi", //TODO: Fix the address if needed
-          "http://localhost:5173" //TODO: REMOVE FROM RELEASE!
+            .allowedOrigins(
+                "https://softala.haaga-helia.fi", // TODO: Fix the address if needed
+                "http://localhost:5173" // TODO: REMOVE FROM RELEASE!
         )
-        .allowedHeaders("Content-Type")
-        .allowedMethods("GET", "POST") //TODO: Allow more methods?
-        .allowedHeaders("application/json")
-        .allowCredentials(false);
-        //.maxAge(3600); //TODO: Change this so that there is validation for weather data?
+            .allowedHeaders("Content-Type")
+            .allowedMethods("GET", "POST") // TODO: Allow more methods?
+            .allowedHeaders("application/json")
+            .allowCredentials(false);
+        // .maxAge(3600); //TODO: Change this so that there is validation for weather
+        // data?
       }
     };
   }
